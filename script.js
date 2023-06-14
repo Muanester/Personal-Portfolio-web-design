044165aa9591c15cb448e30394132c68c7e09734
Muanester <stevenmutisya999@gmail.com>
2023-06-14 22:11:54 +0300

    portfolio_redesign

--- script.js
+++ script.js
@@ -15,42 +15,20 @@ if (clickNav) {
 }
 
 
-// filterObjects('all');
 
-// function filterObjects (c) {
-//     var x, i;
-//     x = document.getElementsByClassName('portBox');
-//     if (c == 'all') c = '';
-//     for (i = 0; i < x.length; i++) {
-//         removeClass(x[i], 'show');
-//         if (x[i].className.indexOf(c) > -1) addClass(x[i], 'show')
-//     }
-// }
 
-// function addClass (element, name) {
-//     var i, arr1, arr2;
-//     arr1 = element.className.split (' ');
-//     arr2 = name.split (' ');
 
-//     for (i = 0; i < arr2.length; i++) {
-//         if (arr1.indexOf(arr2[i]) == -1) {
-//             element.className += ' ' + arr2[i];
-//         }
-//     }
-// }
 
-// function removeClass (element, name) {
-//     var i, arr1, arr2;
-//     arr1 = element.className.split (' ');
-//     arr2 = name.split (' ');
 
-//     for (i = 0; i < arr2.length; i++) {
-//         while (arr1.indexOf(arr2[i]) > -1) {
-//             arr1.splice (arr1.indexOf(arr2[i]), 1);
-//         }
-//     }
 
-//     element.className = arr1.join (' ');
-// }
+// const btnActive = document.querySelector('.portBox')
+// const iconClose = document.querySelector('.icon-close')
 
+
+// btnActive.addEventListener('click', ()=> {
+//     wrapper.classList.add('portActive');
+// });
+// iconClose.addEventListener('click', ()=> {
+//     wrapper.classList.remove('.portActive');
+// });
 
